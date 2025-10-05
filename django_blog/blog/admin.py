2829a2bf4020b888d7e1d4484bb61b4cb99c081f
Nana Safo <iamnanasafo@gmail.com>
2025-10-05 20:01:10 +0000

    django-blog

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author__username', 'published_date')
+
+admin.site.register(Post, PostAdmin)
